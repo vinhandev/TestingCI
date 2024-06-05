acc97acffba999a83a53145bea97d1ef52a6d833
vinhandev <vinhan.dev@gmail.com>
2024-06-05 13:23:26 +0700

    init project

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>CICD</Text>
+    </View>
+  );
+}
+
+export default App;
